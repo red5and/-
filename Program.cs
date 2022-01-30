1baafc8d897d7249c6fe5670348380f47bf1a68d
Lunarian <petrov.reljamy@yandex.ru>
2022-01-30 15:32:36 +0500

    Создал полноценную программу

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿string[] names = {"EKB", "2", "ABOBA", "5"};
-//                  0     1      2      3
-int index = 0;
+﻿var names = new string[] { "ABOBA", ":D", ">:X", "computer", "Ru", "ASDH" }; 
+// находим элементы, у которых длина строки равна 3
+string[] array = Array.FindAll(names, name => name.Length <= 3);
+foreach (var name in array) 
+Console.WriteLine(name);
+
 
